43637459e28e263840cd01b9f8e4b3a4928a7ac4
aman haider <amanhaiderzaidi992727@gmail.com>
2024-06-18 13:07:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const dates=new Date()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Aman Haider</h1>
+      <h2>{dates.getDate()}</h2>
+      <h3>Galgotias University {2*10}</h3>
+    </>
   );
 }
 
